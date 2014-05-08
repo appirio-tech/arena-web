acc2b23260e81b3f21dba5e7eac9f478c075abf3
Mauricio Desiderio <mdesiderio@appirio.com>
2014-05-08 12:46:19 -0500

    adding option to run application without CDN by using localhost

--- cdn.sh
+++
@@ -1,3 +0,0 @@
-# This script pushes the latest web code from the build directory into the CDN repository.
-# It requires the aws CLI to be installed and authorized. You must run aws configure to authorize
-aws s3 sync build/ s3://tc-content/arena/web-v0.1/ --region us-west-1 --acl public-read
