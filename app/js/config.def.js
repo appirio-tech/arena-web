1fac52e380bba799d0b30526e1c5c48b4d82180f
David Hessing <delemach@users.noreply.github.com>
2014-05-19 15:56:40 -0400

    Config fixes

--- app/js/config.def.js
+++ app/js/config.def.js
@@ -1,6 +1,7 @@
 'use strict';
 module.exports = {
     apiDomain : '@@API_DOMAIN',
+    auth0domain: '@@AUTH0_DOMAIN',
     auth0clientID: '@@AUTH0_CLIENT_ID',
     callbackURL: '@@CALLBACK_URL',
     webSocketURL: '@@WEB_SOCKET_URL',
