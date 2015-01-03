d7867c6c03cf5a0bede3d9fffcddc976da63fa1f
Gustavo Costa <gstvac@hotmail.com>
2015-01-03 12:06:11 -0200

    fixes #321 - improvement on resizer code and clicking area height increased

--- app/js/controllers/resizerCtrl.js
+++ app/js/controllers/resizerCtrl.js
@@ -17,63 +17,67 @@
 /*jslint plusplus: true*/
 /*global module, angular, document, $, window*/
 var resizerCtrl = ['$scope', '$element', '$attrs', '$document', '$rootScope', function ($scope, $element, $attrs, $document, $rootScope) {
+    var resizer, resizerTop, resizerBottom, resizerCodeMirror;
+
     function mousemove(event) {
-        var resizer = document.getElementById('resizer'),
-            currentH = resizer.offsetTop - 43,
-            delta = event.clientY - $scope.lastY,
-            newH = currentH + delta;
-        if (newH >= 1 && newH <= $scope.originTotal) {
-            $($attrs.resizerTop).css({
-                height: newH + 'px'
+        var currentH = resizerTop.height();
+        var delta = event.pageY - resizer.offset().top;
+        var newH = currentH + delta;
+
+        if (newH < 1) newH = 1;
+        if (newH > $scope.originTotal) newH = $scope.originTotal;
+
+        resizerTop.css({
+            height: newH + 'px'
+        });
+        resizerBottom.css({
+            height: ($scope.originTotal - newH) + 'px'
+        });
+        if ($scope.topAndCodeMirror >= newH) {
+            resizerCodeMirror.css({
+                height: ($scope.topAndCodeMirror - newH) + 'px'
             });
-            $($attrs.resizerBottom).css({
-                height: ($scope.originTotal - newH) + 'px'
+        } else {
+            resizerCodeMirror.css({
+                height: '0px'
             });
-            if ($scope.topAndCodeMirror >= newH) {
-                $($attrs.resizerCodeMirror).css({
-                    height: ($scope.topAndCodeMirror - newH) + 'px'
-                });
-            } else {
-                $($attrs.resizerCodeMirror).css({
-                    height: '0px'
-                });
-            }
-            $scope.lastY = event.clientY;
-            $scope.cmElem.CodeMirror.refresh();
-            $scope.sharedObj.rebuildErrorBar();
         }
+        $scope.cmElem.CodeMirror.refresh();
+        $scope.sharedObj.rebuildErrorBar();
     }
 
     function mouseup(event) {
         $document.unbind('mousemove', mousemove);
         $document.unbind('mouseup', mouseup);
-        $scope.lastY = undefined;
-        if ($('#bottom-content').css('height') !== 1 ||
-                $('#top-content').css('height') !== 2) {
-            $scope.topStatus = 'normal';
-            $scope.bottomStatus = 'normal';
-        }
+
+        $scope.topStatus = 'normal';
+        $scope.bottomStatus = 'normal';
+
+        if (resizerBottom.height() == 0)
+            $scope.topStatus = 'expand';
+        else if (resizerTop.height() == 1)
+            $scope.bottomStatus = 'expand';
+
         event.preventDefault();
         $rootScope.$broadcast('problem-loaded');
     }
 
     $element.on('mousedown', function (event) {
         event.preventDefault();
+
+        resizer = $('#resizer');
+        resizerTop = $($attrs.resizerTop);
+        resizerBottom = $($attrs.resizerBottom);
+        resizerCodeMirror = $($attrs.resizerCodeMirror);
+
         $document.on('mousemove', mousemove);
         $document.on('mouseup', mouseup);
-        $scope.lastY = event.clientY;
+
         // origin height of top-content: 169(with 1 px padding)
         // origin height of bottom-content: 516
         // origin height of codemirror: 475
-        $scope.originTotal = 685;
-        $scope.topAndCodeMirror = 644;
-        if (window.innerWidth <= 502) {
-            $scope.originTotal = 685 + 60;
-        } else if (window.innerWidth <= 741) {
-            $scope.originTotal = 685 + 30;
-        } else {
-            $scope.originTotal = 685;
-        }
+        $scope.originTotal = resizerTop.height() + resizerBottom.height();
+        $scope.topAndCodeMirror = resizerTop.height() + resizerCodeMirror.height();;
     });
 }];
 module.exports = resizerCtrl;
