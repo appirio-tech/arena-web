ab55ecd321ab7c5e95ad11c11389af9b96ab15fb
Vytautas Helstein <vytautas.helstein@gmail.com>
2015-01-06 14:00:20 +0200

    Added splitter to coding page

--- app/js/controllers/userCodingCtrl.js
+++ app/js/controllers/userCodingCtrl.js
@@ -117,6 +117,12 @@ var userCodingCtrl = ['$scope', '$stateParams', '$rootScope', 'socket', '$window
         $scope.$watch('problemAreaHeightRatio', function () {
             $timeout(function () {
                 $rootScope.$broadcast('problem-loaded');
+                if($scope.cmElem) {
+                    $scope.cmElem.CodeMirror.refresh();
+                }
+                if($scope.sharedObj.rebuildErrorBar) {
+                    $scope.sharedObj.rebuildErrorBar();
+                }
             });
         });
 
