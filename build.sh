6c7d3ebd18223ae041f85150fffea9c32a154474
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2022-08-29 19:00:05 +0530

    build integration

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eo pipefail
+UPDATE_CACHE=""
+
+docker build -f docker/Dockerfile-Build -t $APPNAME:latest \
+--build-arg APPNAME=$APPNAME \
+--build-arg APPENV=$APPENV .
+
+docker create --name app $APPNAME:latest
+docker cp app:/$APPNAME/build .
+
